1a660f5f4fea72ebe98da3a22ace16534838cb49
Kelly Casper <kellyrcasper@gmail.com>
2021-01-07 10:52:09 -0500

    new entry model

---
+++ models/entry.js
@@ -0,0 +1,23 @@
+const {DataTypes} = require('sequelize');
+const db = require('../db');
+
+const Entry = db.define('entry', {
+    entryName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    entryDate: {
+        type: DataTypes.DATE
+    },
+    entryDescription: {
+        type: DataTypes.TEXT,
+    },
+    trip: {
+        type: DataTypes.INTEGER
+    },
+    owner: {
+        type: DataTypes.INTEGER
+    }
+})
+
+module.exports = Entry
