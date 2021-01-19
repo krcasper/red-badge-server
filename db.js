32ef5497aee6a29b24ab9fa55b3e9f9a9ae48ffb
Kelly Casper <kellyrcasper@gmail.com>
2021-01-19 07:03:08 -0500

    made changes for deployment

--- db.js
+++ db.js
@@ -3,6 +3,8 @@ const {Sequelize} = require('sequelize');
 // look back on Array Destructuring notes from Gold Badge
 
 // to complete the parameters for new Sequelize(), we created a 'DB_CONNECTION_STRING' in the .env file. This way, we won't have to put our personal Postgres password into our code where everyone can see it
-const db = new Sequelize(process.env.DB_CONNECTION_STRING);
+const db = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres'
+})
 
 module.exports = db;
